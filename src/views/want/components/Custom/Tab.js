4a26bf82b9ef9f8b66ac8bd954bb73e027bb401b
leeher1123 <leeher1123@naver.com>
2022-01-27 19:15:16 +0900

    tab 페이지 tab 기능 구현

--- src/views/want/components/Custom/Tab.js
+++ src/views/want/components/Custom/Tab.js
@@ -1,14 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import cn from 'classnames';
 
-const Tab = () => (
-  <Container>
-    Tab
-  </Container>
-);
+const Tab = () => {
+  const tabData = ['Tab1', 'Tab2', 'Tab3'];
+  const [currentIndex, setCurrentIndex] = useState();
+  const onClick = (index) => {
+    setCurrentIndex(index);
+  };
+  // eslint-disable-next-line consistent-return
+  const getIndex = () => {
+    if (currentIndex === 0) {
+      return 'Tab menu ONE';
+    }
+    if (currentIndex === 1) {
+      return 'Tab menu TWO';
+    }
+    if (currentIndex === 2) {
+      return 'Tab menu THREE';
+    }
+  };
+  return (
+    <Container>
+      <TabMenu>
+        {
+          tabData.map((item, index) => (
+            <TabItem
+              onClick={() => onClick(index)}
+              className={cn({ isActive: currentIndex === index })}
+            >
+              {item}
+            </TabItem>
+          ))
+        }
+      </TabMenu>
+      <TabContent>
+        {getIndex()}
+      </TabContent>
+    </Container>
+  );
+};
 
 const Container = styled.div`
+  padding-top: 70px;
+`;
 
+const TabMenu = styled.div`
+  width: 650px;
+  height: 43px;
+  padding-left: 15px;
+  display: flex;
+  align-items: center;
+  background: #ddd;
+`;
+
+const TabItem = styled.div`
+  width: 208px;
+  height: 100%;
+  padding-left: 10px;
+  line-height: 43px;
+  color: #767676;
+  cursor: pointer;
+  &.isActive {
+    background-color: rebeccapurple;
+    color: #fff;
+  }
+`;
+
+const TabContent = styled.div`
+  width: 650px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Tab;
